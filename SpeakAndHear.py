a16cbe2bf4bef55ecdf7c73be95bbd5c1058d132
Mike Bee <mbonsign@gmail.com>
2022-04-10 16:32:55 -0500

    testing stream

--- SpeakAndHear.py
+++ SpeakAndHear.py
@@ -1,7 +1,10 @@
+from asyncio import streams
 from speechbrain.pretrained import EncoderDecoderASR
+import pyaudio
 import speechbrain as sb
 import audiofile as af
 import sounddevice as sd
+
 
 # asr_model = EncoderDecoderASR.from_hparams(
 #     source="speechbrain/asr-transformer-transformerlm-librispeech",
@@ -10,11 +13,29 @@ import sounddevice as sd
 # )
 
 
-asr_model = EncoderDecoderASR.from_hparams(
-    source="speechbrain/asr-wav2vec2-commonvoice-en",
-    savedir="pretrained_models/asr-wav2vec2-commonvoice-en",
-    run_opts={"device": "cuda"}
-)
+def init():
+    global asr_model
+    asr_model = EncoderDecoderASR.from_hparams(
+        source="speechbrain/asr-transformer-transformerlm-librispeech",
+        savedir="pretrained_models/asr-transformer-transformerlm-librispeech",
+        run_opts={"device": "cuda:1"}
+    )
+
+
+# asr_model = EncoderDecoderASR.from_hparams(
+#     source="speechbrain/asr-wav2vec2-commonvoice-en",
+#     savedir="pretrained_models/asr-wav2vec2-commonvoice-en",
+#     run_opts={"device": "cuda"}
+# )
+
+
+def listen():
+
+
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1,
+                rate=16000, input=True, frames_per_buffer=1024)
+stream.start_stream()
 
 
 def record_audio(filename, seconds):
