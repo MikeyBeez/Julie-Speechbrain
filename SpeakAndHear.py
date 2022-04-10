3bb90c39447147d339bfcbe69d9a90446ae0e26f
Mike Bee <mbonsign@gmail.com>
2022-04-10 16:40:39 -0500

    First test in branch: Stream

--- SpeakAndHear.py
+++ SpeakAndHear.py
@@ -30,28 +30,34 @@ def init():
 
 
 def listen():
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16, channels=1,
+                    rate=16000, input=True, frames_per_buffer=1024)
+    stream.start_stream()
+    print("* Listening mic. Press Ctrl+C to quit...")
+    while True:
+        stream.start_stream()
+        try:
+            data = stream.read(1024)
+            if len(data) == 0:
+                break
+            result = asr_model.feed_audio(data)
+            print(result)
+        stream.stop_stream()
 
-
-p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1,
-                rate=16000, input=True, frames_per_buffer=1024)
-stream.start_stream()
-
-
-def record_audio(filename, seconds):
-    fs = 16000
-    print("recording {} ({}s) ...".format(filename, seconds))
-    y = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
-    sd.wait()
-    y = y.T
-    af.write(filename, y, fs)
-    print("  ... saved to {}".format(filename))
+# def record_audio(filename, seconds):
+#     fs = 16000
+#     print("recording {} ({}s) ...".format(filename, seconds))
+#     y = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
+#     sd.wait()
+#     y = y.T
+#     af.write(filename, y, fs)
+#     print("  ... saved to {}".format(filename))
 
 
 def main():
     while True:
-        record_audio("temp.wav", 5)
-        print(asr_model.transcribe_file("temp.wav"))
+        listen()
 
 
 main()
