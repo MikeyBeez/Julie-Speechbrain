753f7a692654e5cc5498353f7423f56054b4d161
Mike Bee <mbonsign@gmail.com>
2022-04-11 14:37:50 -0500

    ContinuousListen

--- ContinuousListen.py
+++ ContinuousListen.py
@@ -1,6 +1,10 @@
 #############################################################
+import subprocess
 import pyaudio
 import wave
+import os
+import contextlib
+
 
 def listen():
     WAVE_OUTPUT_FILENAME = "temp.wav"
@@ -8,13 +12,14 @@ def listen():
     p = pyaudio.PyAudio()
     stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
     stream.start_stream()
-    while True:
+    while len(frames) < 40:
         data = stream.read(2000)
         frames.append(data)
         print('length data')
         print(len(data))
-        if len(data) == 0:
-            break
+        # print the number of frames
+        print("* frames: %d" % len(frames))
+
     stream.stop_stream()
     stream.close()
     p.terminate()
@@ -39,6 +44,11 @@ def listen():
 
     os.system("play temp.wav")
 
-listen()
+try:
+    while True:
+        listen()
+except KeyboardInterrupt:
+    pass
+
             
 #############################################################
